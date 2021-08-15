8be0da02823ef209d7e79f94cffd2a6d23277139
sandriannsyafri <sandriannsyafri@gmail.com>
2021-08-15 13:49:44 +0700

    fix

--- 1-syntax-php.php
+++ 1-syntax-php.php
@@ -1,25 +1,37 @@
 
 <?php
 
-//* STANDAR OUTPUT 
+//*==================================
+//* STANDAR OUTPUT ================== 
+//*==================================
 //TODO disable comment to run script;
+
     // echo "Hello World!";
     // print "Hello World!";
 
-//* DEBUG  
+//*==================================
+//* DEBUG =========================== 
+//*==================================
 //TODO disable comment to run script;
+
     $myArrays = [1,2,3,4];
-    var_dump($myArrays);
-    // print_r($myArrays)
+    // var_dump($myArrays);
+    // print_r($myArrays);
 
 
-//* VARIABLE
+//*==================================
+//* VARIABLE ========================
+//*==================================
 //TODO disable comment to run script;
-$message = "Hello Wolrd!";
-// echo $message;
 
-//* OPERATOR
+    $message = "Hello Wolrd!";
+    // echo $message;
+
+//*==================================
+//* OPERATOR ========================
+//*==================================
 //TODO disable comment to run script;
+
     $x = 3;
     $y = 7;
 
@@ -30,7 +42,9 @@ $message = "Hello Wolrd!";
     // $result = $x % $y;
     // echo $result;
 
-//* CONCAT pada Type Data String;
+//*==================================
+//* CONCAT pada Type Data String; ===
+//*==================================
 //TODO disable comment to run script;
 
     $firstName = "Sandrian";
@@ -39,9 +53,15 @@ $message = "Hello Wolrd!";
     // echo $firstName . " " . $lastName;
 
 
-//* OPERATOR ASSIGNMENT;
+//*==================================
+//* OPERATOR ASSIGNMENT =============
+//*==================================
+
 //TODO disable comment to run script;
     $a = 8;
+    $b = 4;
+    $c = 5;
+    $d = 9;
     $sayHello = 'Hello';
 
     // $a *= 2;
@@ -55,7 +75,9 @@ $message = "Hello Wolrd!";
     // echo $a;
     // echo $sayHello;
 
-//* OPERATOR PERBANDINGAN;
+//*==================================
+//* OPERATOR PERBANDINGAN ===========
+//*==================================
 //TODO disable comment to run script;
 
     $number = 9;
@@ -66,7 +88,9 @@ $message = "Hello Wolrd!";
     // var_dump($number == 3);
     // var_dump($number != 3);
 
-//* OPERATOR IDENTITAS;
+//*==================================
+//* OPERATOR IDENTITAS ==============
+//*==================================
 //TODO disable comment to run script;
 
     $myStrings = "1";
@@ -76,7 +100,10 @@ $message = "Hello Wolrd!";
     // var_dump($myStrings !== $myNumber); /** ( !== ) membandingan value dan type data */
     // var_dump($myStrings == $myNumber); /** ( == ) membandingan value nya saja */
 
-//* OPERATOR LOGIKA;
+//*==================================
+//* OPERATOR LOGIKA =================
+//*==================================
+
 //TODO disable comment to run script;
 
     // var_dump(8 > 10 && 2 >= 2); /** false */
@@ -84,12 +111,141 @@ $message = "Hello Wolrd!";
     // var_dump(8 > 10 || 2 >= 2); /** true */
     // var_dump(1 > 10 || 2 !== 2); /** false */
 
-    
-
-
-
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syntax PHP</title>
+    <link rel="stylesheet" href="style.css">
+  
+</head>
+<body>
+    <main>
+        <div class="page-content">
+            <article class="variable">
+                <div class="article-header">
+                    <h1>Output</h1>
+                    <h1>Echo & Print</h1>
+                </div>
+                <div class="article-body">
+                    <table >
+                        <tbody>
+                            <tr>
+                                <td><b>Output from echo </b></td>
+                                <td><?php echo $message ?></td>
+                            </tr>
+                            <tr>
+                                <td><b>Output from print </b></td>
+                                <td><?php print $message ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </article>
+            <article class="operator">
+                <div class="article-header">
+                    <h1>Operator</h1>
+                </div>
+                <div class="article-body">
+                    <table >
+                        <tbody>
+                            <tr>
+                                <td><b>Numbers </b></td>
+                                <td><?php echo $x . "," . $y ?></td>
+                            </tr>
+                            <tr>
+                                <td><b>*</b></td>
+                                <td> x * y =  <?= $x * $y ?></td>
+                            </tr>
+                            <tr>
+                                <td><b>/</b></td>
+                                <td> x / y =  <?= $x / $y ?></td>
+                            </tr>
+                            <tr>
+                                <td><b>+</b></td>
+                                <td> x + y =  <?= $x + $y ?></td>
+                            </tr>
+                            <tr>
+                                <td><b>-</b></td>
+                                <td> x - y =  <?= $x - $y ?></td>
+                            </tr>
+                            <tr>
+                                <td>%</td>
+                                <td>x % 2 =  <?= $x % 2 ?></td>
+                            </tr>
+                          
+                        </tbody>
+                    </table>
+                   
+                </div>
+            </article>
+            <article class="concat">
+                <div class="article-header">
+                    <h1>Concat</h1>
+                </div>
+                <div class="article-body">
+                    <table >
+                        <tbody>
+                            <tr>
+                                <td><b>Tanpa Contact </b></td>
+                                <td><?= $firstName,$lastName ?></td>
+                            </tr>
+                            <tr>
+                                <td><b>Concat </b></td>
+                                <td><?= $firstName . " " . $lastName ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </article>
+            <article class="operator-assignment">
+                <div class="article-header">
+                    <h1>Operator Assigment</h1>
+                </div>
+                <div class="article-body">
+                    <table >
+                        <tbody>
+                            <tr>
+                                <td><b>Numbers </b></td>
+                                <td>
+                                    <ul>
+                                        <li><?php echo "a = $a" ?></li>
+                                        <li><?php echo "b = $b" ?></li>
+                                        <li><?php echo "c = $c" ?></li>
+                                        <li><?php echo "d = $d" ?></li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><b> a *= 10</b></td>
+                                <td><?= $a *= 10 ?></td>
+                            </tr>
+                            <tr>
+                                <td><b> b /= 5</b></td>
+                                <td><?= $b /= 5 ?></td>
+                            </tr>
+                            <tr>
+                                <td><b> c += 4</b></td>
+                                <td><?= $c += 4 ?></td>
+                            </tr>
+                            <tr>
+                                <td><b> d -= 7</b></td>
+                                <td><?= $d -= 7 ?></td>
+                            </tr>
+                          
+                        </tbody>
+                    </table>
+                   
+                </div>
+            </article>
+        </div>
+    </main>
+</body>
+</html>
 
     
 
