5d0f35d449d12e250bcb3a6b18437252faeb4de4
sandriannsyafri <sandriannsyafri@gmail.com>
2021-08-15 13:02:37 +0700

    syntax php

---
+++ 1-syntax-php.php
@@ -0,0 +1,96 @@
+
+<?php
+
+//* STANDAR OUTPUT 
+//TODO disable comment to run script;
+    // echo "Hello World!";
+    // print "Hello World!";
+
+//* DEBUG  
+//TODO disable comment to run script;
+    $myArrays = [1,2,3,4];
+    var_dump($myArrays);
+    // print_r($myArrays)
+
+
+//* VARIABLE
+//TODO disable comment to run script;
+$message = "Hello Wolrd!";
+// echo $message;
+
+//* OPERATOR
+//TODO disable comment to run script;
+    $x = 3;
+    $y = 7;
+
+    // $result = $x * $y;
+    // $result = $x / $y;
+    // $result = $x + $y;
+    // $result = $x - $y;
+    // $result = $x % $y;
+    // echo $result;
+
+//* CONCAT pada Type Data String;
+//TODO disable comment to run script;
+
+    $firstName = "Sandrian";
+    $lastName = "Syafri";
+
+    // echo $firstName . " " . $lastName;
+
+
+//* OPERATOR ASSIGNMENT;
+//TODO disable comment to run script;
+    $a = 8;
+    $sayHello = 'Hello';
+
+    // $a *= 2;
+    // $a /= 2;
+    // $a += 2;
+    // $a -= 2;
+
+    $sayHello .= " ";
+    $sayHello .= "Admin";
+    
+    // echo $a;
+    // echo $sayHello;
+
+//* OPERATOR PERBANDINGAN;
+//TODO disable comment to run script;
+
+    $number = 9;
+    // var_dump($number > 3);
+    // var_dump($number >= 3);
+    // var_dump($number < 3);
+    // var_dump($number <= 3);
+    // var_dump($number == 3);
+    // var_dump($number != 3);
+
+//* OPERATOR IDENTITAS;
+//TODO disable comment to run script;
+
+    $myStrings = "1";
+    $myNumber = "1";
+
+    // var_dump($myStrings === $myNumber); /** ( === ) membandingan value & type data  */
+    // var_dump($myStrings !== $myNumber); /** ( !== ) membandingan value dan type data */
+    // var_dump($myStrings == $myNumber); /** ( == ) membandingan value nya saja */
+
+//* OPERATOR LOGIKA;
+//TODO disable comment to run script;
+
+    // var_dump(8 > 10 && 2 >= 2); /** false */
+    // var_dump(11 > 10 && 2 >= 2); /** true */
+    // var_dump(8 > 10 || 2 >= 2); /** true */
+    // var_dump(1 > 10 || 2 !== 2); /** false */
+
+    
+
+
+
+?>
+
+
+    
+
+
